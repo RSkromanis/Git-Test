8074e5efb7bacaf29a13024f7e8bcc99e7e98931
Renars <art.TJ.246@inbox.lv>
2020-01-24 18:26:58 +0200

    Pirmais GitProject

--- src/gitprojectnew/GitProjectNew.java
+++ src/gitprojectnew/GitProjectNew.java
@@ -17,6 +17,9 @@ public class GitProjectNew {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Sveika, pasaule");
+        System.out.println("Sveika, pasaule");
+        System.out.println("Sveika, pasaule");
+        
     }
     
 }
